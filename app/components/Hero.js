f6ff35fe5e8cc57fb8af2a9a603c7c0595be6311
Diksha Dutta <duttadiksha27@gmail.com>
2025-09-07 17:03:58 +0530

    updated

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -86,14 +86,14 @@ export default function Hero() {
 
             {/* Subtitle */}
             <p className="mt-4 sm:mt-6 text-base sm:text-lg md:text-xl text-[#AB4E52] font-medium">
-              Full Stack Developer • Designer • Creator
+              Full Stack Developer • Designer • Analyst
             </p>
           </motion.div>
         ) : (
           // Phase 2 - split layout
           <motion.div
             key="split"
-            className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-6xl py-8 md:py-12 md:p-20 md:max-w-3xl"
+            className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full lg:max-w-6xl py-8 md:py-12 md:p-20 md:max-w-3xl"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
@@ -101,7 +101,7 @@ export default function Hero() {
           >
             {/* Left content */}
             <motion.div
-              className="flex flex-col justify-center space-y-4 sm:space-y-6 px-4 md:px-0 text-center md:text-left"
+              className="flex flex-col justify-center space-y-4 sm:space-y-6 px-8 md:px-0 text-center md:text-left mx-auto"
               initial={{ opacity: 0, x: 100 }}
               animate={{ opacity: 1, x: 0 }} // x=0 for all screens
               exit={{ opacity: 0, x: 0 }}
@@ -131,19 +131,19 @@ export default function Hero() {
 
             {/* Right content */}
             <motion.div
-              className="flex flex-col items-center justify-center space-y-4 sm:space-y-6 px-4 md:px-0 text-center md:text-left"
+              className="flex flex-col items-center justify-center space-y-4 sm:space-y-6 px-4 md:px-0 text-center md:text-left "
               initial={{ opacity: 0, x: -100 }}
               animate={{ opacity: 1, x: 0 }} // x=0 so no left-right shift on mobile
               exit={{ opacity: 0, x: 0 }}
               transition={{ duration: 1, ease: "easeOut" }}
             >
-              <div className="w-44 sm:w-48 md:w-60 h-auto bg-white shadow-lg rounded-2xl p-4 flex flex-col justify-between mb-4 sm:mb-6 mx-auto md:mx-0">
+              <div className="w-44 sm:w-48 md:w-60 h-auto bg-white/30 shadow-lg rounded-2xl p-4 flex flex-col justify-between mb-4 sm:mb-6 mx-auto md:mx-0">
                 <div>
                   <h3 className="font-bold text-base sm:text-lg">Diksha Dutta</h3>
                   <p className="text-xs sm:text-sm text-gray-500">India</p>
                 </div>
                 <span className="text-xs sm:text-sm font-medium text-[#AB4E52]">
-                  Full Stack Developer
+                  Think. Build. Beautify. Decode.
                 </span>
               </div>
 
